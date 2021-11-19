66bb8e32044e8abb7f7f75353d7e7de00ca2d2bf
Wilson Jesus Martins <68717713+wil-jesus@users.noreply.github.com>
2021-11-19 11:56:27 -0300

    commit components

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
